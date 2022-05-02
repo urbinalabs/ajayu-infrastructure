c5e723111abdfa5462bd974d18b4258ec5edef98
Yamil Urbina <hello@yamilurbina.com>
2022-05-02 03:06:12 -0500

    removing old pipeline

--- bin/ajayu-infrastructure.ts
+++ bin/ajayu-infrastructure.ts
@@ -3,18 +3,6 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Metabase } from '../lib/metabase-stack';
 
-// Pipeline
-import { Pipeline } from '../lib/pipeline';
-
 const app = new cdk.App();
 
-// Metabase
 const metabase = new Metabase(app, 'Metabase');
-
-// Pipeline
-new Pipeline(app, 'Pipeline', {
-  env: {
-    account: '460383082462',
-    region: 'us-east-1'
-  }
-});
